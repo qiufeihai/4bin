a968b197201c2229315884c4e1f0079da7ba0972
qfh <feihai@DESKTOP-5974P1K.localdomain>
2020-12-13 20:15:31 +0800

    add install_4bin.sh 添加从github或gitee安装4bin

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # 4bin
-some script
+
+npm 安装
+-----------------
+npm i -g 4bin
+
+
+github或gitee安装
+------------------------
+# github
+bash <(curl -o- https://raw.githubusercontent.com/qiufeihai/4bin/master/install_4bin.sh)
+# gitee
+bash <(curl -o- https://gitee.com/SImMon_Fo4r/4bin/raw/master/install_4bin.sh)
---
+++ install_4bin.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# author: feihai
+#* 
+# 安装4bin
+#*
+
+install_from_github(){
+  git clone --depth 1 https://github.com/qiufeihai/4bin.git /tmp/4bin
+  chmod +x /tmp/4bin/4* 
+  sudo cp /tmp/4bin/4*  /usr/local/bin/
+  rm -rf /tmp/4bin
+}
+
+install_from_gitee(){
+  git clone --depth 1 https://gitee.com/SImMon_Fo4r/4bin.git /tmp/4bin
+  chmod +x /tmp/4bin/4*
+  sudo cp /tmp/4bin/4*  /usr/local/bin/
+  rm -rf /tmp/4bin
+}
+
+
+PS3='请选择命令: '
+options=(
+  "从github安装"
+  "从gitee安装"
+  "退出"
+)
+select opt in "${options[@]}"
+do
+    case $opt in
+        "从github安装")
+        install_from_github
+            ;;
+        "从gitee安装")
+        install_from_gitee
+            ;;
+        "退出")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
+
